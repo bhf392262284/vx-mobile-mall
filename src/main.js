42214ac9abf6de1cfb62b2d9baf7d8f5718213ea
BiHongBin <410362365@qq.com>
2020-04-07 23:32:14 +0800

    完成首页布局

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App";
 import { request } from "@/utils/index";
+import "@/style/globalStyle.css";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = request;
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,75 @@
 <template>
   <div id="index">
-    <van-tabs active="a">
-      <van-tab title="推荐" name="a">
-        <van-row>
-          <van-col offset="1.5">热门推荐</van-col>
-        </van-row>
-        <van-card num="2" tag="标签" price="10.00" desc="描述信息" title="商品标题" thumb="{{ imageURL }}"></van-card>
+    <van-tabs @change="clickTabs">
+      <van-tab title="推荐">
+        <div class="rap">
+          <div class="Popular">
+            <span>热门推荐</span>
+          </div>
+          <div class="list-describe">
+            <img :src="imageURL" alt />
+            <div class="list-head">
+              <div>
+                <h4>红米8A</h4>
+                <p>5000mAh大电量 / 最高可选4GB+64GB版本</p>
+              </div>
+              <div class="fsize">
+                <i>¥</i>
+                <span>16</span>.00
+              </div>
+            </div>
+          </div>
+          <div class="list-describe">
+            <img :src="imageURL" alt />
+            <div class="list-head">
+              <div>
+                <h4>红米8A</h4>
+                <p>5000mAh大电量 / 最高可选4GB+64GB版本</p>
+              </div>
+              <div class="fsize">
+                <i>¥</i>
+                <span>16</span>.00
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="rap">
+          <div class="Popular">
+            <span>热门推荐</span>
+          </div>
+          <div class="list-describe">
+            <img :src="imageURL" alt />
+            <div class="list-head">
+              <div>
+                <h4>红米8A</h4>
+                <p>5000mAh大电量 / 最高可选4GB+64GB版本</p>
+              </div>
+              <div class="fsize">
+                <i>¥</i>
+                <span>16</span>.00
+              </div>
+            </div>
+          </div>
+          <div class="list-describe">
+            <img :src="imageURL" alt />
+            <div class="list-head">
+              <div>
+                <h4>红米8A</h4>
+                <p>5000mAh大电量 / 最高可选4GB+64GB版本</p>
+              </div>
+              <div class="fsize">
+                <i>¥</i>
+                <span>16</span>.00
+              </div>
+            </div>
+          </div>
+        </div>
       </van-tab>
-      <van-tab title="手机" name="b">手机</van-tab>
-      <van-tab title="电视" name="c">电视</van-tab>
-      <van-tab title="笔记本" name="c">笔记本</van-tab>
-      <van-tab title="家电" name="c">家电</van-tab>
-      <van-tab title="居家" name="c">居家</van-tab>
+      <van-tab title="手机">手机</van-tab>
+      <van-tab title="电视">电视</van-tab>
+      <van-tab title="笔记本">笔记本</van-tab>
+      <van-tab title="家电">家电</van-tab>
+      <van-tab title="居家">居家</van-tab>
     </van-tabs>
   </div>
 </template>
@@ -21,15 +79,72 @@ export default {
   name: "index",
   data() {
     return {
-      imageURL: "/static/img/ipad.jpeg"
+      imageURL: "../../static/img/ipad.jpeg"
     };
   },
-  methods: {},
-  created() {
-    // let app = getApp()
-  }
+  methods: {
+    clickTabs(e) {
+      console.log(e.mp.detail);
+    }
+  },
+  created() {}
 };
 </script>
 
 <style scoped>
+#index .list-describe {
+  box-sizing: border-box;
+  padding: 8px 16px;
+  display: flex;
+}
+#index .Popular {
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 10px 16px;
+  font-size: 14px;
+}
+#index .Popular::after {
+  content: "";
+  border-bottom: 1px solid #ebedf0;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 16px;
+  transform: scaleY(0.5);
+}
+#index .rap {
+  background-color: #fff;
+}
+#index .list-describe img {
+  height: 88px;
+  width: 88px;
+  background-color: red;
+}
+#index .list-describe .list-head {
+  display: flex;
+  margin-left: 6px;
+  font-size: 12px;
+  flex-direction: column;
+  justify-content: space-between;
+}
+#index .list-describe .list-head p {
+  padding-top: 2px;
+}
+#index .list-describe .list-head h4 {
+  color: #323233;
+}
+#index .list-describe .list-head p {
+  color: #646566;
+}
+#index .list-describe .list-head span {
+  flex-shrink: 1;
+}
+.fsize span {
+  display: inline-block;
+  font-size: 16px;
+}
+.fsize i {
+  display: inline-block;
+}
 </style>
