c312c1fc516a0aeddc2de08fc507d5ba2b52119a
BiHongBin <410362365@qq.com>
2020-05-06 23:20:29 +0800

    新增了商品取消

--- src/pages/myOrder/index.vue
+++ src/pages/myOrder/index.vue
@@ -1,294 +1,133 @@
 <template>
   <div class="myOrder">
-    <van-tabs>
-      <van-tab title="全部">
-        <div class="productDetails">
-          <div class="productDetails-top">
-            <div class="shop-list-tuik">
-              <p>订单编号</p>
-              <p class="font">已退款</p>
-            </div>
-            <div class="shop-dingdan">
-              <p>365461564151451566146</p>
-            </div>
-          </div>
-          <div class="productDetails-content">
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="heji">
-              <p>合计:7499</p>
-            </div>
-          </div>
-          <div class="productDetails-footer">
-            <van-button color="#ee0a24" type="danger" size="small">退款</van-button>
-          </div>
-        </div>
-      </van-tab>
-      <van-tab title="待付款">
-        <div class="productDetails">
-          <div class="productDetails-top">
-            <div class="shop-list-tuik">
-              <p>订单编号</p>
-              <p class="font">待付款</p>
-            </div>
-            <div class="shop-dingdan">
-              <p>365461564151451566146</p>
-            </div>
-          </div>
-          <div class="productDetails-content">
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="heji">
-              <p>合计:7499</p>
-            </div>
-          </div>
-          <div class="productDetails-footer">
-            <van-button type="default" size="small">取消订单</van-button>&nbsp; &nbsp;
-            <van-button color="#ee0a24" type="danger" size="small">立即付款</van-button>
-          </div>
-        </div>
-      </van-tab>
-      <van-tab title="待发货">
-        <div class="productDetails">
-          <div class="productDetails-top">
-            <div class="shop-list-tuik">
-              <p>订单编号</p>
-              <p class="font">待发货</p>
-            </div>
-            <div class="shop-dingdan">
-              <p>365461564151451566146</p>
-            </div>
-          </div>
-          <div class="productDetails-content">
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="heji">
-              <p>合计:7499</p>
-            </div>
-          </div>
-          <div class="productDetails-footer">
-            <van-button color="#ee0a24" type="danger" size="small">代发货</van-button>
-          </div>
-        </div>
-      </van-tab>
-      <van-tab title="已发货">
-        <div class="productDetails">
-          <div class="productDetails-top">
-            <div class="shop-list-tuik">
-              <p>订单编号</p>
-              <p class="font">已发货</p>
-            </div>
-            <div class="shop-dingdan">
-              <p>365461564151451566146</p>
-            </div>
-          </div>
-          <div class="productDetails-content">
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="heji">
-              <p>合计:7499</p>
-            </div>
-          </div>
-          <div class="productDetails-footer">
-            <van-button color="#ee0a24" type="danger" size="small">确认收货</van-button>
-          </div>
-        </div>
-      </van-tab>
-      <van-tab title="已完成">
-        <div class="productDetails">
-          <div class="productDetails-top">
-            <div class="shop-list-tuik">
-              <p>订单编号</p>
-              <p class="font">已完成</p>
-            </div>
-            <div class="shop-dingdan">
-              <p>365461564151451566146</p>
-            </div>
-          </div>
-          <div class="productDetails-content">
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="shop-img">
-              <img />
-              <div class="right-content">
-                <div class="kuan">
-                  <p>游戏本2019款</p>
-                  <p class="p2">游戏本2019款</p>
-                </div>
-                <div class="Price">
-                  <i>￥7499</i>
-                  <i class="chen1">x1</i>
-                </div>
-              </div>
-            </div>
-            <div class="heji">
-              <p>合计:7499</p>
-            </div>
-          </div>
-          <div class="productDetails-footer">
-            <van-button color="#ee0a24" type="danger" size="small">已完成</van-button>
-          </div>
-        </div>
-      </van-tab>
+    <van-tabs @change="whole">
+      <van-tab :title="data.title" v-for="(data,i) in tabData" :key="i"></van-tab>
     </van-tabs>
+    <div class="productDetails" v-for="(item,index) in commodity" :key="index">
+      <div class="productDetails-top">
+        <div class="shop-list-tuik">
+          <p>订单编号</p>
+          <p class="font">{{item.statusName}}</p>
+        </div>
+        <div class="shop-dingdan">
+          <p>{{item.orderSn}}</p>
+        </div>
+      </div>
+      <div class="productDetails-content" v-for="(items,s) in item.items " :key="s">
+        <div class="shop-img">
+          <img :src="imgUrl + items.goods.pic" />
+          <div class="right-content">
+            <div class="kuan">
+              <p>{{items.title}}</p>
+              <p class="p2">{{items.goods.descript}}</p>
+            </div>
+            <div class="Price">
+              <i>￥{{(items.price) / 100}}.00</i>
+              <i class="chen1">x{{items.count }}</i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="heji">
+        <p>合计:{{(item.totalPrice) / 100}}.00</p>
+      </div>
+      <div class="productDetails-footer">
+        <van-button
+          color="#ee0a24"
+          type="default"
+          size="small"
+          v-if="item.payStatus===1 && item.status ===1"
+          @click="cancellationOfOrder(item.orderSn)"
+        >取消订单</van-button>&nbsp; &nbsp;
+        <van-button
+          color="#ee0a24"
+          type="danger"
+          size="small"
+          v-if="item.payStatus===1 && item.status ===1"
+        >立即费款</van-button>
+        <van-button
+          color="#ee0a24"
+          type="danger"
+          size="small"
+          v-if="item.payStatus === 1 && item.status === 5"
+        >已取消</van-button>
+        <van-button
+          color="#ee0a24"
+          type="danger"
+          size="small"
+          v-if="item.payStatus === 2 && item.status === 2"
+        >待发货</van-button>
+        <van-button
+          color="#ee0a24"
+          type="danger"
+          size="small"
+          v-if="item.payStatus === 2 && item. status === 3"
+        >确认收货</van-button>
+        <van-button
+          color="#ee0a24"
+          type="danger"
+          size="small"
+          v-if="item.payStatus === 2 && item. status === 4"
+        >已完成</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { baseImageUrl } from "@/utils/index";
 export default {
   name: "myOrder",
   data() {
-    return {};
+    return {
+      imgUrl: baseImageUrl,
+      commodity: [],
+      tabIndex: 0,
+      tabData: [
+        {
+          title: "全部"
+        },
+        {
+          title: "待付款"
+        },
+        {
+          title: "待发货"
+        },
+        {
+          title: "已发货"
+        },
+        {
+          title: "已完成"
+        }
+      ]
+    };
   },
-  methods: {},
+  methods: {
+    whole(e) {
+      let index = e.mp.detail.index;
+      this.tabIndex = index;
+      this.wholes(index);
+    },
+    wholes(index) {
+      // 订单列表
+      this.$http("user/order/getOrders", "get", {
+        page: 1,
+        limit: 50,
+        status: index
+      }).then(res => {
+        this.commodity = res.data.records;
+      });
+    },
+    cancellationOfOrder(orderSn) {
+      this.$http("user/order/cancel/" + orderSn, "post").then(res => {
+        this.wholes(this.tabIndex);
+      });
+    }
+  },
 
-  onShow() {}
+  onShow() {
+    this.wholes(0);
+  }
 };
 </script>
 
@@ -318,9 +157,6 @@ export default {
 }
 .shop-list-tuik .font {
   color: red;
-}
-.productDetails-content {
-  position: relative;
 }
 .productDetails-footer {
   padding: 10px 16px;
@@ -355,12 +191,13 @@ export default {
   font-size: 12px;
 }
 .heji {
+  position: relative;
   padding: 10px;
   font-size: 16px;
   display: flex;
   justify-content: flex-end;
 }
-.productDetails-content::after {
+.heji::after {
   position: absolute;
   bottom: 0;
   content: "";
